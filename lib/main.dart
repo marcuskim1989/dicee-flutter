9be51d492c8cbcd2a4e2a820a1c1dbf01ed988f0
Marcus Kim <marcuskim1989@gmail.com>
2021-07-11 14:20:40 -0700

    Finish UI, test out buttons

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,33 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  print('Left dice pressed');
+                },
+                child: Image.asset('images/dice1.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  print('Right dice pressed');
+                },
+                child: Image.asset('images/dice1.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
